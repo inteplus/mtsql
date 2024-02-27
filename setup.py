4cd66e7f30539f188a630ea962861ea3cf4ce9af
Minh-Tri <minhtri.pham@winnowsolutions.com>
2024-02-27 15:43:10 +0000

    v1.11: supporting pyproject.toml

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import os
 from setuptools import setup, find_packages, find_namespace_packages
-from mt.sql.version import version
+
+VERSION_FILE = os.path.join(os.path.dirname(__file__), "VERSION.txt")
 
 setup(
     name="mtsql",
-    version=version,
     description="Extra Python modules to deal with the interaction between pandas dataframes and remote SQL servers, for Minh-Tri Pham",
     author=["Minh-Tri Pham"],
     packages=find_packages() + find_namespace_packages(include=["mt.*"]),
@@ -22,5 +23,14 @@ setup(
     project_urls={
         "Documentation": "https://mtdoc.readthedocs.io/en/latest/mt.sql/mt.sql.html",
         "Source Code": "https://github.com/inteplus/mtsql",
+    },
+    setup_requires=["setuptools-git-versioning<2"],
+    setuptools_git_versioning={
+        "enabled": True,
+        "version_file": VERSION_FILE,
+        "count_commits_from_version_file": True,
+        "template": "{tag}",
+        "dev_template": "{tag}.dev{ccount}+{branch}",
+        "dirty_template": "{tag}.post{ccount}",
     },
 )
