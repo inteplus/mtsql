b3bb91911bca0de46287235f7b36109c1b6442b4
Minh-Tri Pham <pmtri80@gmail.com>
2025-01-16 10:21:38 +0000

    registering our own connectors

--- mt/sql/redshift/__init__.py
+++ mt/sql/redshift/__init__.py
@@ -1,3 +1,6 @@
+from pkg_resources import DistributionNotFound, get_distribution, parse_version
+from sqlalchemy.dialects import registry  # noqa
+
 from .main import *
 
 __api__ = [
@@ -15,3 +18,26 @@ __api__ = [
     "drop_column",
     "conform",
 ]
+
+
+for package in ["psycopg2", "psycopg2-binary", "psycopg2cffi"]:
+    try:
+        if get_distribution(package).parsed_version < parse_version("2.5"):
+            raise ImportError("Minimum required version for psycopg2 is 2.5")
+        break
+    except DistributionNotFound:
+        pass
+
+registry.register("rs", "mt.sql.redshift.dialect", "RedshiftDialect_psycopg2")
+registry.register("rs.psycopg2", "mt.sql.redshift.dialect", "RedshiftDialect_psycopg2")
+registry.register(
+    "rs+psycopg2cffi",
+    "mt.sql.redshift.dialect",
+    "RedshiftDialect_psycopg2cffi",
+)
+
+registry.register(
+    "rs+redshift_connector",
+    "mt.sql.redshift.dialect",
+    "RedshiftDialect_redshift_connector",
+)
